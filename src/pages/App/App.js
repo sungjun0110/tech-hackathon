7ee427094948170ea2151703929889c05e609d6e
robertthome <remedy42290@gmail.com>
2022-05-17 12:08:42 -0700

    Reward Modal card created (non-functional)

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,16 +1,18 @@
-import { useState } from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
-import Home from './Home';
-import './App.css';
+import { useState } from 'react'
+import { Routes, Route, Link } from 'react-router-dom'
+import Home from './Home'
+import RewardModal from './RewardModal'
+import './App.css'
 
 function App() {
-	return (
-		<div className='App'>
-			<Routes>
-				<Route path='/' element={<Home />} />
-			</Routes>
-		</div>
-	);
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <RewardModal />
+    </div>
+  )
 }
 
-export default App;
+export default App
