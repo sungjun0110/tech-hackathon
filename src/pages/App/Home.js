8efd4225a22047845d9cbefd20134f8fdc2b45e9
brockpaperscissors <michaeljbrock91@gmail.com>
2022-05-16 20:09:34 -0700

    Home component sculpted out and css classnames added

--- src/pages/App/Home.js
+++ src/pages/App/Home.js
@@ -1,66 +1,86 @@
 export default function Home() {
 	return (
 		<>
-			<div>
-				<header>
-					<div>
-						<h1>Build-a-Farm</h1>
-						<h3>Math Problems</h3>
+			<div className='home-container'>
+				<header className='home-header'>
+					<div className='home-title-container'>
+						<h1 className='home-title'>Build-a-Farm</h1>
+						<h3 className='home-sub'>Math Problems</h3>
 					</div>
-					<div>
-						<p>Sign Up</p>
-						<p>Sign In</p>
+					<div className='home-title-container'>
+						<p className='home-sign'>Sign Up</p>
+						<p className='home-sign'>Sign In</p>
 					</div>
 				</header>
-				<nav>
-					<div>
-						<div>
-							<button>K</button>
+				<nav className='home-nav-container'>
+					<div className='home-grade-container'>
+						<div className='home-grade-button'>
+							<button className='button'>K</button>
 							<p>Kinder</p>
 						</div>
-						<div>
-							<button>1</button>
+						<div className='home-grade-button'>
+							<button className='button'>1</button>
 							<p>1st Grade</p>
 						</div>
-						<div>
-							<button>2</button>
+						<div className='home-grade-button'>
+							<button className='button'>2</button>
 							<p>2nd Grade</p>
 						</div>
-						<div>
-							<button>3</button>
+						<div className='home-grade-button'>
+							<button className='button'>3</button>
 							<p>3rd Grade</p>
 						</div>
-						<div>
-							<button>4</button>
+						<div className='home-grade-button'>
+							<button className='button'>4</button>
 							<p>4th Grade</p>
 						</div>
-						<div>
-							<button>5</button>
+						<div className='home-grade-button'>
+							<button className='button'>5</button>
 							<p>5th Grade</p>
 						</div>
-						<div>
-							<button>6</button>
+						<div className='home-grade-button'>
+							<button className='button'>6</button>
 							<p>6th Grade</p>
 						</div>
 					</div>
 					<div>
-						<button>Word Problems</button>
-						<button>Addition</button>
-						<button>Subtraction</button>
-						<button>Multiplication</button>
-						<button>Division</button>
-						<button>Fractions</button>
-						<button>Ratios</button>
+						<button className='home-subject-button'>Word Problems</button>
+						<button className='home-subject-button'>Addition</button>
+						<button className='home-subject-button'>Subtraction</button>
+						<button className='home-subject-button'>Multiplication</button>
+						<button className='home-subject-button'>Division</button>
+						<button className='home-subject-button'>Fractions</button>
+						<button className='home-subject-button'>Ratios</button>
 					</div>
 				</nav>
-				<main>
-					<div>
-						<h2>Cow Mania</h2>
-						<img src='' alt='Cow Mania' />
-						<p>Complete addition problems to feed the cow.</p>
+				<main className='home-main-container'>
+					<div className='home-hero-container'>
+						<h2 className='home-hero-title'>Cow Mania</h2>
+						<img src='' alt='Cow Mania' className='home-hero-image' />
+						<p className='home-hero-description'>
+							Complete addition problems to feed the cow.
+						</p>
 					</div>
-					<button>PLAY</button>
+					<button className='home-button-play'>PLAY</button>
 				</main>
+				<footer className='home-footer-container'>
+					<div className='hfg'>
+						<img src='' alt='Chicken Count' className='hfg-image' />
+						<p className='hfg-description'>Chicken Count</p>
+					</div>
+					<div className='hfg'>
+						<img src='' alt='Pigs in a Blanket' className='hfg-image' />
+						<p className='hfg-description'>Pigs in a Blanket</p>
+					</div>
+					<div className='hfg'>
+						<img src='' alt='Horse Roundup' className='hfg-image' />
+						<p className='hfg-description'>Horse Roundup</p>
+					</div>
+					<div className='hfg'>
+						<img src='' alt='Sleepy Sheep' className='hfg-image' />
+						<p className='hfg-description'>Sleepy Sheep</p>
+					</div>
+				</footer>
 			</div>
 		</>
 	);
