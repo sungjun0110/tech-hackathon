cc8da96392ea0cdc55c1b37c93e200441b2d3298
aaayon91 <aaayon91@gmail.com>
2022-05-17 11:58:54 -0700

    adds user display component

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 import Home from './Home';
 import './App.css';
-import KeyPad from '../../components/KeyPad/KeyPad';
 
 function App() {
 	return (
