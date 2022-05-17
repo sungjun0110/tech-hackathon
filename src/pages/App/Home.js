65f91fce28016bb9513d3897db54642bd679aa81
brockpaperscissors <michaeljbrock91@gmail.com>
2022-05-16 19:41:24 -0700

    initial Home component created

---
+++ src/pages/App/Home.js
@@ -0,0 +1,67 @@
+export default function Home() {
+	return (
+		<>
+			<div>
+				<header>
+					<div>
+						<h1>Build-a-Farm</h1>
+						<h3>Math Problems</h3>
+					</div>
+					<div>
+						<p>Sign Up</p>
+						<p>Sign In</p>
+					</div>
+				</header>
+				<nav>
+					<div>
+						<div>
+							<button>K</button>
+							<p>Kinder</p>
+						</div>
+						<div>
+							<button>1</button>
+							<p>1st Grade</p>
+						</div>
+						<div>
+							<button>2</button>
+							<p>2nd Grade</p>
+						</div>
+						<div>
+							<button>3</button>
+							<p>3rd Grade</p>
+						</div>
+						<div>
+							<button>4</button>
+							<p>4th Grade</p>
+						</div>
+						<div>
+							<button>5</button>
+							<p>5th Grade</p>
+						</div>
+						<div>
+							<button>6</button>
+							<p>6th Grade</p>
+						</div>
+					</div>
+					<div>
+						<button>Word Problems</button>
+						<button>Addition</button>
+						<button>Subtraction</button>
+						<button>Multiplication</button>
+						<button>Division</button>
+						<button>Fractions</button>
+						<button>Ratios</button>
+					</div>
+				</nav>
+				<main>
+					<div>
+						<h2>Cow Mania</h2>
+						<img src='' alt='Cow Mania' />
+						<p>Complete addition problems to feed the cow.</p>
+					</div>
+					<button>PLAY</button>
+				</main>
+			</div>
+		</>
+	);
+}
