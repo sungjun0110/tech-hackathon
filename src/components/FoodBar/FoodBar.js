a28940096dd05e5e2d9b946ed228f8e599a601e3
BrockPaperScissors <michaeljbrock91@gmail.com>
2022-05-17 16:43:38 -0700

    Created working foodbar function, cowmania game graphics added

---
+++ src/components/FoodBar/FoodBar.js
@@ -0,0 +1,15 @@
+import "./FoodBar.css";
+export default function FoodBar() {
+	return (
+		<>
+			<div className="food-bar-controller">
+				<div className="food-bar-container">
+					<div className="food-bar-background">
+						<div className="food-bar"></div>
+					</div>
+				</div>
+				<h3 className="food">Food</h3>
+			</div>
+		</>
+	);
+}
--- src/pages/App/CowMania.js
+++ src/pages/App/CowMania.js
@@ -1,3 +1,17 @@
+import FoodBar from "../../components/FoodBar/FoodBar";
+import GameContainer from "../../components/GameContainer/GameContainer";
+import "./CowMania.css";
 export default function CowMania() {
-	return <div>Hello from Cow Mania!</div>;
+	return (
+		<div className="cowmania-page">
+			<div className="cowmania-container">
+				<div className="cowmania-gamecontainer">
+					<GameContainer />
+				</div>
+				<div cowmania-foodbar>
+					<FoodBar />
+				</div>
+			</div>
+		</div>
+	);
 }
