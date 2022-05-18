c02f98669b3c65c531358012cd9064b6febef7fd
robertthome <remedy42290@gmail.com>
2022-05-17 22:04:48 -0700

    updated package-lock.json

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,23 +1,25 @@
-import GameDetails from "./GameDetails";
-import CowMania from "./CowMania";
-import ComingSoon from "./ComingSoon";
-import "./App.css";
-import { useState } from "react";
-import { Routes, Route, Link } from "react-router-dom";
-import Home from "./Home";
-import "./App.css";
+import GameDetails from './GameDetails'
+import CowMania from './CowMania'
+import ComingSoon from './ComingSoon'
+import './App.css'
+import { useState } from 'react'
+import { Routes, Route, Link } from 'react-router-dom'
+import Home from './Home'
+import RewardModal from './RewardModal'
+import './App.css'
 
 function App() {
-	return (
-		<div className="App">
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/gamedetails" element={<GameDetails />} />
-				<Route path="/cowmania" element={<CowMania />} />
-				<Route path="/*" element={<ComingSoon />} />
-			</Routes>
-		</div>
-	);
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/gamedetails" element={<GameDetails />} />
+        <Route path="/cowmania" element={<CowMania />} />
+        <Route path="/*" element={<ComingSoon />} />
+      </Routes>
+      <RewardModal />
+    </div>
+  )
 }
 
-export default App;
+export default App
--- src/pages/App/RewardModal.js
+++ src/pages/App/RewardModal.js
@@ -6,24 +6,55 @@ const Backdrop = () => {
   return <div></div>
 }
 
-const ModalOverlay = (props) => {}
+const ModalOverlay = () => {}
 
-const RewardModal = () => {
+const RewardModal = (props) => {
   return (
-    <Card className="card">
-      <h2 className="congrats">Congratulations</h2>
-      <p className="msg">You've earned a new barn animal!</p>
-      <img src="" alt="Star Trophy"></img>
-      <div className="btn_container">
-        <button name="button" type="button" className="btn view_btn">
-          View Barn
-        </button>
-        <button type="button" className="btn next_btn">
-          Next Level
-        </button>
-      </div>
-    </Card>
+    <div>
+      <div className="backdrop" />
+      <Card className="card">
+        <h2 className="congrats">Congratulations</h2>
+        {/* <h2> {props.title} </h2> */}
+        <p className="msg">You've earned a new barn animal!</p>
+        {/* <p> {props.message} </p> */}
+        <img
+          className="star_img"
+          src="https://i.imgur.com/SQjlB3J.png"
+          alt="Star Trophy"
+        ></img>
+        <div className="btn_container">
+          <button name="button" type="button" className="btn view_btn">
+            View Barn
+          </button>
+          <button type="button" className="btn next_btn">
+            Next Level
+          </button>
+        </div>
+      </Card>
+    </div>
   )
 }
 
 export default RewardModal
+
+// inside game logic file
+// 1: Will need to manage reward State
+//  const [reward, setReward] = useState()
+//--------------------------------
+
+// 2: manage state inside a game winning handler with conditional
+//  if (gameWon)
+//  setReward({
+//    title: 'Congratulations'
+//    message: 'You've earned a barn animal!'
+// })
+//--------------------------------
+
+// 3: rendor the modal when winning state is true
+// {reward && <RewardModal title={reward.title} message={reward.message}>}
+//---------------------------------
+
+// 4a: Have button restart the game
+// 4b: make modal dismissible by clicking on back drop or exit button????
+// a: clear winning state when button is clicked
+//
